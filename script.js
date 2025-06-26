60c509e1d19156404f6e617593ec5e53ba29574b
ColdAsianrice <dphan4206@gmail.com>
2025-06-26 01:07:21 -0500

    Done a lot to the contact me, added a nav button so you can go back to the top!

--- script.js
+++ script.js
@@ -4,10 +4,19 @@ let restart = document.querySelector('#replay');
 let intro = document.querySelector('.intro');
 let content = document.querySelector('.content');
 let aboutme = document.querySelector('.aboutme');
-let aboutButton = document.querySelector('#memoi')
+let contactme = document.querySelector('.contactingme');
 
+let aboutButton = document.querySelector('#memoi')
+let contactButton = document.querySelector('#contact');
 let titleScreen = document.querySelector('.title-screen');
+let navButton = document.querySelector('#navbutton');
 
 aboutButton.addEventListener('click', function(){
     aboutme.scrollIntoView({behavior: "smooth"});
+});
+contactButton.addEventListener('click', function(){
+    contactme.scrollIntoView({behavior: "smooth"});
+});
+navButton.addEventListener('click', function(){
+    titleScreen.scrollIntoView({behavior: "smooth"});
 });
